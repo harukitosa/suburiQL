ed18785a430699a88ab4b4a335f62f8579c7369c
harukitosa <13haruki28@gmail.com>
2021-05-05 16:08:54 +0900

    change gqlgen

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module suburiQL
 
 go 1.16
 
-require github.com/graphql-go/graphql v0.7.9 // indirect
+require (
+	github.com/99designs/gqlgen v0.13.0 // indirect
+	github.com/graphql-go/graphql v0.7.9 // indirect
+	github.com/vektah/gqlparser/v2 v2.1.0 // indirect
+)
---
+++ graph/schema.resolvers.go
@@ -0,0 +1,33 @@
+package graph
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"context"
+	"fmt"
+	"suburiQL/graph/generated"
+	"suburiQL/graph/model"
+)
+
+func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
+	todo := model.Todo{ID: "1", Text: "totototodo"}
+	todo2 := model.Todo{ID: "2", Text: "hogehogehogheo"}
+	var t []*model.Todo
+	t = append(t, &todo)
+	t = append(t, &todo2)
+	return t, nil
+}
+
+// Mutation returns generated.MutationResolver implementation.
+func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
+
+// Query returns generated.QueryResolver implementation.
+func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
+
+type mutationResolver struct{ *Resolver }
+type queryResolver struct{ *Resolver }
